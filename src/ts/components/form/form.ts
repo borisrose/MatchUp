499af2d3181b954a07f38901354d07767d928bd6
Boris Rose <boris.alexandre.rose@gmail.com>
2024-07-19 10:55:27 +0200

    fix/form.ts

--- src/ts/components/form/form.ts
+++ src/ts/components/form/form.ts
@@ -18,3 +18,26 @@ interface Input {
 }
 
 // vous avez vu que dans la définition de cette interface j'utilise d'autres interfaces en l'occurrence Input et Button pour dire que la propriété inputs sera un tableau composé d'objets implémentant l'interface Input et pour dire que buttons sera un tableau d'objets implémentant l'interface Button
+interface FormProps {
+  formId: string
+  inputs: Input[]
+  buttons: Button[]
+}
+
+export default function (d: FormProps) {
+  return `
+        <form id="${d.formId}">
+            <section class="form__inputs">
+                <!-- 
+                
+                    ceci est un commentaire : ce qui se passe en-dessous est la chose suivant : je demande à ce que pour chaque (each) élément du tableau inputs , propriété de d , soit créé un composant formField à qui je passe l'élément du tableau et vu que le résultat de map est un tableau j'utilise la méthode join et je lui passe comme valeur "" pour éviter qu'une virgule soit mise entre les différents composants puisque par principe les éléments d'un tableau sont séparés par une ,  
+                
+                -->
+                ${d.inputs.map((el: Input) => formField(el)).join("")}
+            </section>
+             <section class="form__buttons">
+                ${d.buttons.map((el: Button) => button(el)).join("")}
+            </section>
+        </form>    
+    `
+}
