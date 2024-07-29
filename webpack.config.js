b27d17ec26e8143912ee9e1a4057ed2b524b017a
borisrose <boris.alexandre.rose@gmail.com>
2024-07-29 02:05:34 +0200

    fix/bug

--- webpack.config.js
+++ webpack.config.js
@@ -19,11 +19,6 @@ module.exports = {
             {
                 test: /.ts$/,
                 use: "ts-loader",
-                exclude: "/node_modules"
-            },
-            {
-                test: /.css$/,
-                use: ["style-loader", "css-loader"]
             },
             {
                 test: /.s[ac]ss$/,
